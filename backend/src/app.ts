430f633e116522add3a4643b9349ae636d0ffc1e
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-05-11 03:01:08 +0530

    changes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 // Middleware
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
+    "http://localhost:3000",
     "https://note-taking-app-1.onrender.com"
   ],
   credentials: true,
