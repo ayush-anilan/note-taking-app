eec93a35223777501175ff607018fb2198d1fd92
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-05-11 00:25:58 +0530

    changes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ const app: Application = express();
 app.use(cors({
   origin: [
     "https://note-taking-app-1.onrender.com",
-    "http://localhost:5000"
+    "http://localhost:5173"
   ],
   credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
