4e965a5eb33e12ab796af975cad3f1b3bc43a644
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-05-10 23:46:31 +0530

    changes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,10 +25,10 @@ app.use('/api', routes);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, '../../../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 
