2066f01a9dfcf0eee60fa807824c2dd55fb1d232
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-05-11 11:50:12 +0530

    chaned api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_NODE_ENV === "development"
-      ? "http://localhost:3000/api"
-      : import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
